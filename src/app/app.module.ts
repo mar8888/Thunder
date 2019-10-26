9f9480f2ea369b305d72451ca7d54c3294eb1d74
Paco <paco.sanchez.ram@gmail.com>
2019-10-26 11:54:04 -0500

    Component generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IdInputComponent } from './id-input/id-input.component';
+import { InfoDisplayComponent } from './info-display/info-display.component';
+import { ProjectComponent } from './info-display/project/project.component';
+import { HoursComponent } from './info-display/hours/hours.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IdInputComponent,
+    InfoDisplayComponent,
+    ProjectComponent,
+    HoursComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/id-input/id-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-id-input',
+  templateUrl: './id-input.component.html',
+  styleUrls: ['./id-input.component.css']
+})
+export class IdInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
