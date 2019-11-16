b0297ba07254eafdd0cabc23d6bf63f281c28ea3
Paco <paco.sanchez.ram@gmail.com>
2019-11-16 11:57:45 -0600

    update

--- src/app/shared/students.service.ts
+++ src/app/shared/students.service.ts
@@ -1,3 +1,34 @@
-export class students {
+export class StudentsService {
+    readXlsxFile = require('read-excel-file');
+    schema = {
+        'Matricula': {
+            prop: 'matricula',
+            type: String
+        },
+        'Ciudadano': {
+            prop: 'ciudadano',
+            type: String
+        },
+        'Profesional': {
+            prop: 'profesional',
+            type: String
+        },
+        'Proyecto': {
+            prop: 'proyecto',
+            type: String
+        },
+        'Organizacion': {
+            prop: 'organizacion',
+            type: String
+        }
+    }
 
+    fetchData() {
+        this.readXlsxFile('', this.schema).then(({rows, errors}) => {
+            if(errors.length === 0) {
+                
+            }
+
+        })
+    }
 }
